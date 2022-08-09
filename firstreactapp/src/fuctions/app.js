0123a84a7737df489e0a470d2cc899b9db41a6b1
amrutha <amrutha.1@globallogic.com>
2022-08-09 20:01:57 +0530

    Functions

---
+++ firstreactapp/src/fuctions/app.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Main from './main';
+import Name from './name'
+import Web from './web';
+
+
+function App()
+{
+    return(
+        <>
+        {/* <Main></Main> */}
+        {/* <Name></Name> */}
+        <Web></Web>
+        </>
+
+    )
+}
+export default App
